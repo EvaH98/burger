28f459b2d88bdfa86098038daab1591fcb62364c
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-05-09 18:55:36 -0700

    idk pt2

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
 
 if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        user: " upqjngbz0bracnqp",
-        password: " upqjngbz0bracnqp",
-        database: "acsejklgupan455e"
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
     });
-}
+};
 
 connection.connect(function(err) {
     if (err) {
