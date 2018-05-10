407293e528c2545650925bfd2bb4a9f755db38d7
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-05-09 18:47:01 -0700

    idk

--- config/connection.js
+++ config/connection.js
@@ -12,10 +12,10 @@ if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "burgers_db"
+        host: "ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: " upqjngbz0bracnqp",
+        password: " upqjngbz0bracnqp",
+        database: "acsejklgupan455e"
     });
 }
 
